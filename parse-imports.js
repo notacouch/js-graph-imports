432e44c2ca84d7ffd65c2f3f04f827b0a1c6c26f
Bruno Monteiro <brunoalencar@gmail.com>
2015-11-12 10:54:26 -0200

    Update parse-imports.js

--- parse-imports.js
+++ parse-imports.js
@@ -1,17 +1,13 @@
 function parseImports(content) {
-  var importRe = /\@import ([.\s\S]+?);?$/g;
-  var depRe = /["'](.+?)["']/g;
-  var importMatch = {};
-  var depMatch = {};
-  var results = [];
-  // strip comments
-  content = new String(content).replace(/\/\*.+?\*\/|\/\/.*(?=[\n\r])/g, '');
-  while ((importMatch = importRe.exec(content)) !== null) {
-    while ((depMatch = depRe.exec(importMatch[1])) !== null) {
-      results.push(depMatch[1]);
+    var regex = /([\/]{2}|[\/][*]).?import.?[(]?.?["'](.*)["'].?[)]?[;]?.*?(\n[*][\/])?/g;
+    var match = {};
+    var results = [];
+
+    while (match = regex.exec(content)) {
+        results.push(match[2]);
     }
-  }
-  return results;
+
+    return results;
 }
 
 module.exports = parseImports;
