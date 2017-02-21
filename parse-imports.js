c417e566a36c46381d11bf6c3e9e2f3a0e94104e
Abed Islam <versatil@gmail.com>
2017-02-21 14:29:28 -0500

    Added support for importing via @codekit-prepend and -appends.

--- parse-imports.js
+++ parse-imports.js
@@ -1,10 +1,10 @@
 function parseImports(content) {
-    var regex = /([\/]{2}|[\/][*]).?import.?[(]?.?["'](.*)["'].?[)]?[;]?.*?(\n[*][\/])?/g;
+    var regex = /([\/]{2}|[\/][*]).?(import|@codekit-prepend|@codekit-append).?[(]?.?["'](.*)["'].?[)]?[;]?.*?(\n[*][\/])?/g;
     var match = {};
     var results = [];
 
     while (match = regex.exec(content)) {
-        results.push(match[2]);
+        results.push(match[3]);
     }
 
     return results;
